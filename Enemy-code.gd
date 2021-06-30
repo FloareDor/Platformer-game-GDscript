100753689761e8a37e13b3ddb6340afdb941f49a
Sai Ravi Teja Gangavarapu <73626236+SaiRaviTejaG-gif@users.noreply.github.com>
2021-07-01 00:48:23 +0530

    Added Dead State and Enemy Animations.

--- Enemy-code.gd
+++ Enemy-code.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 var BULLET_SCENE = preload("res://Characters/Bullet.tscn")
+onready var animatedSprite = get_node("AnimatedSprite")
+
 
 var GRAVITY = 10
 const SPEED = 30
@@ -12,7 +14,8 @@ var count = rpm
 const rpm = 100
 enum {  # list
 	IDLE,
-	ATTACK
+	ATTACK,
+	DEAD
 }
 var state = IDLE
 func _ready():
@@ -26,20 +29,40 @@ func _physics_process(delta): # delta = time that the last fram took to process
 			idle_state(delta)
 		ATTACK:
 			attack_state(delta)	
+		DEAD:
+			dead_state(delta)
+func dead_state(delt):
+	velocity = Vector2.ZERO
 func idle_state(delta):
 	velocity = Vector2.ZERO
 	velocity.x = SPEED * direction
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
+	
 	if is_on_wall():
 		direction = direction * -1
 		$FallOffCast.position.x *= -1
+		if animatedSprite.flip_h == false:
+			animatedSprite.flip_h = true
+		else:
+			animatedSprite.flip_h = false
 	if $FallOffCast.is_colliding() == false:
 		direction = direction * -1
 		$FallOffCast.position.x *= -1
+		if animatedSprite.flip_h == false:
+			animatedSprite.flip_h = true
+		else:
+			animatedSprite.flip_h = false
+	animatedSprite.animation = "Run"
 	
 func attack_state(delta):
-	#var real_player = get_node("/root/World/player")
+	#var real_player = get_node("/root/Wssorld/player")
+	
+	animatedSprite.animation = "Idle"
+	if player.position.x > self.position.x:
+		animatedSprite.flip_h = false
+	else:
+		animatedSprite.flip_h = true
 	velocity = Vector2.ZERO
 	velocity.y += GRAVITY
 	if player != null:
@@ -48,6 +71,7 @@ func attack_state(delta):
 		if is_on_floor():
 			velocity = move_and_slide(velocity, FLOOR)
 	if count == 0:
+		#animatedSprite.animation = "Attack"
 		fire()
 		count = rpm
 	count+=-1
@@ -57,14 +81,16 @@ func attack_state(delta):
 	
 func _on_Area2D_body_entered(body):
 	print("found him")
-	if body != self:
+	if body != self and state != DEAD:
 		player = body
 		state = ATTACK
 func _on_Area2D_body_exited(body):
 	player = null
-	state = IDLE
+	if state != DEAD:
+		state = IDLE
 
 func fire():
+	animatedSprite.animation = "Attack"
 	var bullet = BULLET_SCENE.instance()
 	bullet.position = get_position()
 	bullet.player = player
@@ -77,4 +103,9 @@ func fire():
 
 
 func _on_hurtbox_body_entered(body):
-	queue_free()
+	animatedSprite.animation = "HitandDie"
+	if state == DEAD:
+		queue_free()
+	else:
+		state = DEAD
+	
